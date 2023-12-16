702eee22122392b569b27d24d7f1b1b1dc9b3d64
Prasad Babu <pbabu11088@gmail.com>
2023-12-16 17:13:27 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+ import  {useState} from 'react';
+//  import './index.css'
+ 
+ const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
 
-function App() {
+
+  const handleAddTodo = () => {
+    if (newTodo.trim() !== "") {
+      setTodos([...todos, { text: newTodo.trim(), checked: false }])
+      setNewTodo("");
+    };
+  }
+  const handleDeleteTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos)
+
+  };
+  const handleToggleTodo = (index) => {
+    const newTodos = [...todos]
+    newTodos[index].checked = !newTodos[index].checked;
+    setTodos(newTodos)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className='container'>
+      <h2>To-Do List </h2>
+      <input type="text" value={newTodo} onChange={(e) => setNewTodo(e.target.value)}
+ placeholder="Enter a task..." />
+ 
 
-export default App;
+<button style={{backgroundColor:"black",color:"white",borderRadius:"5px",padding:"5px"}} onClick={handleAddTodo}>Add</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index} style={{ display: "flex" }}>
+            <div style={{ display: "flex",justifyContent:"center", alignItems: "center" }}>
+
+
+              <input type="checkbox"
+                checked={todo.checked}
+                onChange={() => handleToggleTodo(index)}
+              />
+              <span
+                style={{ marginRight: "10px", textDecoration: todo.checked ? "Line-through" : "none", }}>{todo.text}</span>
+              <button style={{ marginTop: "5px", marginBottom: "5px",backgroundColor:"black",color:"white",borderRadius:"5px",padding:"5px"}} type="submit" onClick={() => handleDeleteTodo(index)}>Delete</button>
+            </div>
+
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+ 
+ export default App;
